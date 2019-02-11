0adca397ad29ca380538b2de6eb28268ab931bb8
hehaijin <hehaijin1981@gmail.com>
2019-02-10 22:12:57 -0700

    uploading function works

--- src/app/password-manger/password-manger.component.ts
+++ src/app/password-manger/password-manger.component.ts
@@ -37,24 +37,27 @@ export class PasswordMangerComponent implements OnInit {
       if (!result) return;
       console.log('The dialog was closed');
       console.log(result);
-      this.allPasswordData= this.parseResult(result);
+      this.allPasswordData = this.parseResult(result);
     });
   }
 
-  parseResult(result){
-    return  [{
-      groupName: 'work',
-      content: [
-        new PasswordItem(),
-        new PasswordItem(),
-      ]
-    }, {
-      groupName: 'RDP',
-      content: [
-        new PasswordItem(),
-        new PasswordItem(),
-      ]
-    }];
+  parseResult(result) {
+    let groups = new Set();
+    for (let item of result) {
+      groups.add(item.group || "default");
+    }
+    let finalGroups = [];
+    for (let group of Array.from(groups)) {
+      let thisGroup = []
+      for (let item of result) {
+        if (item.group === group) {
+          thisGroup.push(item);
+        }
+      }
+      finalGroups.push({ groupName: group, content: thisGroup });
+    }
+    console.log(finalGroups);
+    return finalGroups;
   }
 
 }
